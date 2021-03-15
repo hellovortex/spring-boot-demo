36f9f15a4006b554936080798093f6f88a685a33
zqs <zqszmd@sian.com>
2021-03-15 19:11:00 +0800

    redis-transaction

---
+++ demo-redis-transaction/src/main/java/com/xkcoding/redis/transaction/SpringBootDemoRedisTransactionApplication.java
@@ -0,0 +1,11 @@
+package com.xkcoding.redis.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoRedisTransactionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoRedisTransactionApplication.class, args);
+    }
+}
